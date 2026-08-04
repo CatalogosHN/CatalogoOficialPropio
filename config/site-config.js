b7de3e1b37d6c5bb2d976fc8517d22f53174a574
CatalogosHN <sakuratop2025xxx@gmail.com>
2026-08-03 21:02:46 -0600

    Add files via upload

---
+++ config/site-config.js
@@ -0,0 +1,7 @@
+window.SITE_CONFIG = {
+  appsScriptUrl: "PEGAR_AQUI_URL_DE_APPS_SCRIPT",
+  whatsappNumber: "50496310102",
+  freeShippingThreshold: 2500,
+  storeName: "International Items HN",
+  homeUrl: "/"
+};
